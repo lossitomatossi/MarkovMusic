f49090d669dc0122f9565133f292e05462e6be2f
Tomas Terälä <35957699+lossitomatossi@users.noreply.github.com>
2021-09-18 21:35:38 +0300

    Tiedostonlukija paranneltu
    Palauttaa nyt String:in sijaan List<String> josta pystyy helpommin tuottamaan Bigrameja, joista saa laskettua todennäköisyydet Markovin ketjuun.

--- MarkovMusic/src/main/java/MarkovMusic/Apumetodit/Tiedostonlukija.java
+++ MarkovMusic/src/main/java/MarkovMusic/Apumetodit/Tiedostonlukija.java
@@ -5,20 +5,36 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author tompp
  */
 public class Tiedostonlukija {
+    String nuottiRegex;
     
 
     public Tiedostonlukija() {
+        nuottiRegex = "[a-zA-Z0-9]+";
     }
     
-    public String lataaNuotitTekstista(String kappale) throws IOException {
+    public List<String> lataaNuotitTekstista(String kappale) throws IOException {
         //tee monipuolinen metodi
-        return this.lueTiedosto("musiikki/sointutiedostot/Wonderwall.txt");
+        String teksti = this.lueTiedosto("musiikki/sointutiedostot/Wonderwall.txt");
+        return tekstiNuoteiksi(teksti);
+    }
+    
+    private List<String> tekstiNuoteiksi(String kappale) {
+        List<String> kaikkiNuotit = new ArrayList<>();
+        Matcher m = Pattern.compile(nuottiRegex).matcher(kappale);
+        while (m.find()) {
+            kaikkiNuotit.add(m.group());
+        }
+        return kaikkiNuotit;
     }
     
     private String lueTiedosto(String sijainti) throws FileNotFoundException, IOException {
--- MarkovMusic/src/main/java/MarkovMusic/Kayttolittyma/Kayttoliittyma.java
+++ MarkovMusic/src/main/java/MarkovMusic/Kayttolittyma/Kayttoliittyma.java
@@ -5,7 +5,9 @@ package MarkovMusic.Kayttolittyma;
  * @author tompp
  */
 
+import MarkovMusic.Algoritmit.ParinMuodostaja;
 import MarkovMusic.Apumetodit.Tiedostonlukija;
+import MarkovMusic.Tietorakenteet.Bigram;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,11 +18,17 @@ public class Kayttoliittyma {
     //private final kappaleet();
     private final BufferedReader br;
     private final Tiedostonlukija tl;
-    private List<String> kappaleet;
+    private List<List<String>> kappaleet;
+    private List<List<Bigram>> bigramit;
+    private ParinMuodostaja pm;
+    
     
     public Kayttoliittyma() {
         br = new BufferedReader(new InputStreamReader(System.in));
         tl = new Tiedostonlukija();
+        kappaleet = new ArrayList<>();
+        bigramit = new ArrayList<>();
+        pm = new ParinMuodostaja();
     }
     
     public void kaynnistaKayttoliittyma() throws IOException {
@@ -49,10 +57,27 @@ public class Kayttoliittyma {
                     System.out.println("Valitse tiedosto");
                     //komento = br.readLine();
                     //komento = komento.toLowerCase();
-                    String wonderwall = tl.lataaNuotitTekstista("Wonderwall");
+                    List<String> wonderwall = tl.lataaNuotitTekstista("Wonderwall");
                     kappaleet.add(wonderwall);
                     break;
                 case "bigram":
+                    System.out.println("Olet valinnut " + kappaleet.size() + " kappaletta. Haluatko tehda niista bigram parit? (K/E)");
+                    komento = br.readLine();
+                    komento = komento.toLowerCase();
+                    switch (komento) {
+                        case "k":
+                            for (List<String> kappale : kappaleet) {
+                                bigramit.add(pm.muodostaNuottiParit(kappale));
+                            }
+                            System.out.println("Bigram parit muodostettu!");
+                            //tee metodi joka muutta bigram parit uniikeiks ja laskee lukumäärät et saa todennäkösyydet.
+                            break;
+                        case "e":
+                            break;
+                        default:
+                            System.out.println("Komentoa " + komento + "ei tunnistettu. Valitse K tai E.");
+                            break;
+                    }
                     break;
                 default:
                     System.out.println("Komentoa " + komento + " ei ole olemassa.\n"
