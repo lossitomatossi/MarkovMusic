83f74c3faa23f3a45bf44e8717675fe1928fe692
Tomas Terälä <35957699+lossitomatossi@users.noreply.github.com>
2021-09-23 22:30:50 +0300

    sovelluslogiikkaa Triesolmuille

--- MarkovMusic/src/main/java/MarkovMusic/Tietorakenteet/TrieSolmu.java
+++ MarkovMusic/src/main/java/MarkovMusic/Tietorakenteet/TrieSolmu.java
@@ -7,12 +7,32 @@ import java.util.HashMap;
  * @author tompp
  */
 public class TrieSolmu {
-    private HashMap<Float, TrieSolmu> lapset;
+    private HashMap<Double, String> lapset;
     private String avain;
+    private Boolean numero;
 
-    public TrieSolmu(String avain) {
+    public TrieSolmu(String avain, Boolean onNumero) {
         this.avain = avain;
         this.lapset = new HashMap();
+        this.numero = onNumero;
+    }
+    
+    
+    //ottaa vastaan kaikki lapset ja niiden esiintymislukumaarat
+    public void lisaaLapset(HashMap<String, Integer> lisattavat) {
+        Integer esiintymia = 0;
+        for (Integer lkm : lisattavat.values()) {
+            esiintymia += lkm;
+        }
+        
+        Double todnak = 0.0;
+        Double edellisenTodnak = 0.0;
+        Integer lkm;
+        for (String nuotti : lisattavat.keySet()) {
+            lkm = lisattavat.get(nuotti);
+            todnak = edellisenTodnak + (lkm / esiintymia);
+            this.lapset.put(todnak, nuotti);
+        }
     }
     
     
