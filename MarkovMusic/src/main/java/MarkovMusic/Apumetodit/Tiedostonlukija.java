676996e01fdfbb55e5aa186f908cdeaad320a6f7
Tomas Terälä <35957699+lossitomatossi@users.noreply.github.com>
2021-09-25 23:55:35 +0300

    Parannettu testikattavuutta ja lisätty testitiedostoja

--- MarkovMusic/src/main/java/MarkovMusic/Apumetodit/Tiedostonlukija.java
+++ MarkovMusic/src/main/java/MarkovMusic/Apumetodit/Tiedostonlukija.java
@@ -5,57 +5,67 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 /**
  *
  * @author tompp
  */
 public class Tiedostonlukija {
+
     String nuottiRegex;
-    
 
     public Tiedostonlukija() {
         nuottiRegex = "[a-zA-Z0-9]+";
     }
-    
+
     public List<String> lataaNuotitTekstista(String kappale) throws IOException {
-        //tee monipuolinen metodi
-        String teksti;
-        if (kappale.equals("testi")) {
-            teksti = this.lueTiedosto("musiikki/sointutiedostot/testitiedosto.txt");
-        } else {
-            teksti = this.lueTiedosto("musiikki/sointutiedostot/Wonderwall.txt");
-        }
+        String sijainti = "musiikki/sointutiedostot/" + kappale + ".txt";
+        String teksti = this.lueTiedosto(sijainti);
+
         return tekstiNuoteiksi(teksti);
-        
     }
-    
+
     public List<String> tekstiNuoteiksi(String kappale) {
         List<String> kaikkiNuotit = new ArrayList<>();
         Matcher m = Pattern.compile(nuottiRegex).matcher(kappale);
         while (m.find()) {
             kaikkiNuotit.add(m.group());
         }
+
         return kaikkiNuotit;
     }
-    
+
     public String lueTiedosto(String sijainti) throws FileNotFoundException, IOException {
         File tiedosto = new File(sijainti);
         BufferedReader br = new BufferedReader(new FileReader(tiedosto));
         String teksti = "";
         String rivi;
-        while(true) {
+        while (true) {
             rivi = br.readLine();
             if (rivi == null) {
                 break;
             }
-            teksti = teksti.concat(rivi);
+            teksti = teksti.concat(rivi + " ");
         }
-        
+
         return teksti;
+    }
+
+    public List<String> listaaTiedostot(String sijainti) throws IOException {
+        File kansio = new File(sijainti);
+        List<String> tiedostot = new ArrayList();
+        for (File tiedosto : kansio.listFiles()) {
+            tiedostot.add(tiedosto.getName());
+        }
+
+        return tiedostot;
     }
 }
--- MarkovMusic/src/test/java/MarkovMusic/Apumetodit/TiedostonlukijaTest.java
+++ MarkovMusic/src/test/java/MarkovMusic/Apumetodit/TiedostonlukijaTest.java
@@ -9,9 +9,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 
 /**
  *
@@ -25,30 +27,38 @@ public class TiedostonlukijaTest {
     
     public TiedostonlukijaTest() {
     }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
     
     @Before
     public void setUp() {
         tl = new Tiedostonlukija();
-        testiSijainti = "musiikki/sointutiedostot/testitiedosto.txt";
-        testiTeksti = "a a a b b b";
-        List<String> oletettuLista = new ArrayList<>();
-        Collections.addAll(oletettuLista, "a", "a", "a", "b", "b", "b");
+        testiSijainti = "musiikki/testikansio/testitiedosto.txt";
+        testiTeksti = "Em A Em A Em A";
+        oletettuLista = new ArrayList<>();
+        Collections.addAll(oletettuLista, "Em", "A", "Em", "A", "Em", "A");
     }
     
     @After
     public void tearDown() {
     }
 
-//    /**
-//     * Testaa metodia lataaNuotitTekstista luokka: Tiedostonlukija.
-//     */
-//    @Test
-//    public void testLataaNuotitTekstista() throws Exception {
-//        System.out.println("lataaNuotitTekstista");
-//        String kappale = "testi";
-//        List<String> tulos = tl.lataaNuotitTekstista(kappale);
-//        assertEquals(oletettuLista, tulos);
-//    }
+    /**
+     * Testaa metodia lataaNuotitTekstista luokka: Tiedostonlukija.
+     */
+    @Test
+    public void testLataaNuotitTekstista() throws Exception {
+        System.out.println("lataaNuotitTekstista");
+        String kappale = "painotus";
+        List<String> tulos = tl.lataaNuotitTekstista(kappale);
+        assertEquals(oletettuLista, tulos);
+    }
    
     /**
      * Testaa metodia tekstiNuoteiksi, luokka: Tiedostonlukija
@@ -56,7 +66,7 @@ public class TiedostonlukijaTest {
     @Test
     public void testTekstiNuoteiksi() {
         List<String> oletetut = new ArrayList<>();
-        Collections.addAll(oletetut, "a", "a", "a", "b", "b", "b");
+        Collections.addAll(oletetut, "Em", "A", "Em", "A", "Em", "A");
         List<String> testattavat = tl.tekstiNuoteiksi(testiTeksti);
         assertEquals(oletetut, testattavat);
     }
@@ -67,15 +77,31 @@ public class TiedostonlukijaTest {
     @Test
     public void testLueTiedosto() throws FileNotFoundException, IOException {
         BufferedReader br = new BufferedReader(new FileReader(new File(testiSijainti)));
-        String oletettu = "";
-        String rivi;
-        while(true) {
-            rivi = br.readLine();
-            if (rivi == null) {
-                break;
-            }
-            oletettu = oletettu.concat(rivi);
-        }
+        String oletettu = "a a a b b b ";
+        
         assertEquals(oletettu, tl.lueTiedosto(testiSijainti));
+    }
+
+    /**
+     * Test of listaaTiedostot method, of class Tiedostonlukija.
+     */
+    @Test
+    public void testListaaTiedostot() throws Exception {
+        System.out.println("listaaTiedostot");
+        String sijainti = "musiikki/testikansio/testi2";
+        tl.listaaTiedostot(sijainti);
+        ArrayList<String> tyhja = new ArrayList<>();
+        assertEquals(tyhja, tl.listaaTiedostot(sijainti));
+    }
+    
+    @Test
+    public void testListaaTiedostot2() throws Exception {
+        System.out.println("listaaTiedostot");
+        String sijainti = "musiikki/testikansio";
+        tl.listaaTiedostot(sijainti);
+        ArrayList<String> vertailu = new ArrayList<>();
+        vertailu.add("testi2");
+        vertailu.add("testitiedosto.txt");
+        assertEquals(vertailu, tl.listaaTiedostot(sijainti));
     }
 }
