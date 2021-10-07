4e03000dae89c4e061b2970b6a829ac093431475
Tomas Terälä <35957699+lossitomatossi@users.noreply.github.com>
2021-10-07 20:05:49 +0300

    testiluokat Trielle ja TrieSolmulle

---
+++ MarkovMusic/src/test/java/MarkovMusic/Tietorakenteet/TrieSolmuTest.java
@@ -0,0 +1,26 @@
+package MarkovMusic.Tietorakenteet;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tompp
+ */
+public class TrieSolmuTest {
+
+    public TrieSolmuTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
---
+++ MarkovMusic/src/test/java/MarkovMusic/Tietorakenteet/TrieTest.java
@@ -0,0 +1,27 @@
+package MarkovMusic.Tietorakenteet;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tompp
+ */
+public class TrieTest {
+    
+    public TrieTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+
+//    @Test
+//    public void testSomeMethod() {
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+}
