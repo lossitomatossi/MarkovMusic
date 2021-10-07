165d6c0101103a400d2723972464b4f397220eb5
Tomas Terälä <35957699+lossitomatossi@users.noreply.github.com>
2021-10-07 20:05:38 +0300

    TrieSolmulle kuuluva koodi poistettu Triestä

--- MarkovMusic/src/main/java/MarkovMusic/Tietorakenteet/Trie.java
+++ MarkovMusic/src/main/java/MarkovMusic/Tietorakenteet/Trie.java
@@ -8,45 +8,10 @@ import java.util.Random;
  */
 public class Trie {
 
-    private final TrieSolmu[] lapset;
-    private final int[] lukumaarat;
-    private int yhteensa;
-    private TrieSolmu apusolmu;
-    Random r;
+    
+    private TrieSolmu juuri;
 
     public Trie() {
-        this.lapset = new TrieSolmu[128];
-        this.lukumaarat = new int[128];
-        this.yhteensa = 0;
-        this.apusolmu = new TrieSolmu();
-        this.r = new Random();
-    }
-
-    public void lisaaLapsi(int x) {
-        apusolmu = lapset[x];
-        if (apusolmu == null) {
-            lapset[x] = new TrieSolmu();
-        }
-        this.yhteensa++;
-        lukumaarat[x]++;
-    }
-    
-    public int juureenLisatty() {
-        return yhteensa;
-    }
-    
-    public int satunnaisluku() {
-        return r.nextInt(yhteensa);
-    }
-    
-    public TrieSolmu valitseSolmu() {
-        int x = satunnaisluku();
-        for (int i = 0; i < lapset.length; i++) {
-            x -= lukumaarat[i];
-            if (x <= 0) {
-                return lapset[i];
-            }
-        }
-        return null;
+        this.juuri = new TrieSolmu();
     }
 }
--- MarkovMusic/src/main/java/MarkovMusic/Tietorakenteet/TrieSolmu.java
+++ MarkovMusic/src/main/java/MarkovMusic/Tietorakenteet/TrieSolmu.java
@@ -1,14 +1,49 @@
 package MarkovMusic.Tietorakenteet;
 
-import java.util.HashMap;
+import java.util.Random;
 
 /**
  *
  * @author tompp
  */
 public class TrieSolmu {
-    
+    private final TrieSolmu[] lapset;
+    private final int[] lukumaarat;
+    private int yhteensa;
+    Random r;
 
     public TrieSolmu() {
+        this.lapset = new TrieSolmu[128];
+        this.lukumaarat = new int[128];
+        this.yhteensa = 0;
+        this.r = new Random();
+    }
+    
+    public void lisaaLapsi(int x) {
+        
+        if (lapset[x] == null) {
+            lapset[x] = new TrieSolmu();
+        }
+        this.yhteensa++;
+        lukumaarat[x]++;
+    }
+    
+    public int juureenLisatty() {
+        return yhteensa;
+    }
+    
+    public int satunnaisluku() {
+        return r.nextInt(yhteensa);
+    }
+    
+    public TrieSolmu valitseSolmu() {
+        int x = satunnaisluku();
+        for (int i = 0; i < lapset.length; i++) {
+            x -= lukumaarat[i];
+            if (x <= 0) {
+                return lapset[i];
+            }
+        }
+        return null;
     }
 }
