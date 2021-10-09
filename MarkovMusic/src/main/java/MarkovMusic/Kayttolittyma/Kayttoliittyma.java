1da1b03e74eddee45eada0b67eb13a9c8df76555
Tomas Terälä <35957699+lossitomatossi@users.noreply.github.com>
2021-10-09 18:05:12 +0300

    Markovin ketjut listoista

--- MarkovMusic/src/main/java/MarkovMusic/Kayttolittyma/Kayttoliittyma.java
+++ MarkovMusic/src/main/java/MarkovMusic/Kayttolittyma/Kayttoliittyma.java
@@ -11,12 +11,12 @@ import MarkovMusic.Apumetodit.MIDsoitin;
 import MarkovMusic.Apumetodit.Tiedostonlukija;
 import MarkovMusic.Tietorakenteet.Bigram;
 import MarkovMusic.Tietorakenteet.Trie;
-import MarkovMusic.Tietorakenteet.TrieSolmu;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -121,18 +121,18 @@ public class Kayttoliittyma {
                     lista[0] = 1;
                     lista[1] = 3;
                     lista[2] = 6;
-                    t.lisaaTriehen(lista);
+                    t.lisaaTaulukkoTriehen(lista);
 
                     int[] lista2 = new int[3];
                     lista2[0] = 1;
                     lista2[1] = 3;
                     lista2[2] = 2;
-                    t.lisaaTriehen(lista2);
+                    t.lisaaTaulukkoTriehen(lista2);
 
                     lista2[0] = 2;
                     lista2[1] = 2;
                     lista2[2] = 2;
-//                    t.lisaaTriehen(lista2);
+//                    t.lisaaTaulukkoTriehen(lista2);
                     System.out.println(t.etsiJonoa(lista2));
                     System.out.println(t.etsiJonoa(lista));
                     int[] lista3 = new int[2];
@@ -140,6 +140,33 @@ public class Kayttoliittyma {
                     lista3[1] = 3;
                     System.out.println(t.etsiJonoa(lista3));
 
+                    break;
+                case "n":
+                    MIDItiedot eka = new MIDItiedot(0, 3, 5);
+                    MIDItiedot toka = new MIDItiedot(0, 4, 7);
+                    MIDItiedot kolmas = new MIDItiedot(0, 8, 3);
+                    MIDItiedot neljas = new MIDItiedot(0, 3, 3);
+                    MIDItiedot viides = new MIDItiedot(0, 4, 3);
+                    MIDItiedot kuudes = new MIDItiedot(0, 4, 3);
+
+                    List<MIDItiedot> midit = new ArrayList<>();
+                    Collections.addAll(midit, eka, toka, kolmas, neljas, viides, kuudes);
+                    Trie testiT = new Trie();
+                    testiT.lisaaMiditiedotTriehen(midit, 3);
+                    break;
+                case "b":
+                    eka = new MIDItiedot(0, 3, 5);
+                    toka = new MIDItiedot(0, 4, 7);
+                    kolmas = new MIDItiedot(0, 8, 3);
+                    neljas = new MIDItiedot(0, 3, 3);
+                    viides = new MIDItiedot(0, 4, 3);
+                    kuudes = new MIDItiedot(0, 4, 3);
+                    MIDItiedot seiska = new MIDItiedot(0, 4, 3);
+
+                    midit = new ArrayList<>();
+                    Collections.addAll(midit, eka, toka, kolmas, neljas, viides, kuudes, seiska);
+                    testiT = new Trie();
+                    testiT.lisaaMiditiedotTriehen(midit, 4);
                     break;
                 default:
                     System.out.println("Komentoa " + komento + " ei ole olemassa.\n"
--- MarkovMusic/src/main/java/MarkovMusic/Tietorakenteet/Trie.java
+++ MarkovMusic/src/main/java/MarkovMusic/Tietorakenteet/Trie.java
@@ -1,5 +1,8 @@
 package MarkovMusic.Tietorakenteet;
 
+import MarkovMusic.Apumetodit.MIDItiedot;
+import java.util.List;
+
 /**
  *
  * @author tompp
@@ -16,7 +19,7 @@ public class Trie {
      *
      * @param lista
      */
-    public void lisaaTriehen(int[] lista) {
+    public void lisaaTaulukkoTriehen(int[] lista) {
         int[] kopio = lista;
 
         TrieSolmu nykyinen = juuri;
@@ -27,8 +30,8 @@ public class Trie {
                 nykyinen.lisaaSolmu(luku);
             }
 
-            nykyinen.printtaaTiedot();
-            System.out.println(" ");
+//            nykyinen.printtaaTiedot();
+//            System.out.println(" ");
             nykyinen = nykyinen.palautaSolmu(luku);
         }
     }
@@ -43,7 +46,44 @@ public class Trie {
                 return false;
             }
         }
-
         return true;
+    }
+
+    public void lisaaMiditiedotTriehen(List<MIDItiedot> lista, int syvyys) {
+        int koko = lista.size();
+        int jakojaannos = koko % syvyys;
+        int apuIndeksi = koko - jakojaannos;
+
+        System.out.println("Syvyys: " + syvyys + " listan koko: " + koko + " jakojaannos: "
+                + jakojaannos + " apuIndeksi: " + apuIndeksi);
+        int j = 0;
+        int[] lisattavat = new int[syvyys];
+        for (int i = 0; i < apuIndeksi; i++) {
+
+            if (j == syvyys) {
+                lisaaTaulukkoTriehen(lisattavat);
+                j = 0;
+            }
+            System.out.println("J on: " + j + ", i on: " + i);
+            lisattavat[j++] = lista.get(i).getSavel();
+        }
+        if (jakojaannos > 0) {
+            lisaaLoputMidit(lista, apuIndeksi, j, syvyys);
+        }
+    }
+
+    private void lisaaLoputMidit(List<MIDItiedot> lista, int apuIndeksi, int j, int syvyys) {
+        int koko = lista.size();
+        int jakojaannos = koko % syvyys;
+        if (j == syvyys) {
+            System.out.println("Nollataan välissä");
+            j = 0;
+        }
+        int[] lisattavat2 = new int[jakojaannos + 1];
+        for (int i = apuIndeksi; i < koko; i++) {
+            System.out.println("J on: " + j + " i on: " + i);
+            lisattavat2[j++] = lista.get(i).getSavel();
+        }
+        lisaaTaulukkoTriehen(lisattavat2);
     }
 }
